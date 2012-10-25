0ca5e39bfc3df5a0fc149989d656bf5175ecf728
juilus <juliustch@gmail.com>
2012-10-25 12:54:07 -0400

    add util function

---
+++ socintpy/util/utils.py
@@ -0,0 +1,11 @@
+'''
+some utility functions.
+'''
+
+def convert_to_utf8_str(arg):
+  # written by Michael Norton (http://docondev.blogspot.com/)
+  if isinstance(arg, unicode):
+    arg = arg.encode('utf-8')
+  elif not isinstance(arg, str):
+     arg = str(arg)
+  return arg
