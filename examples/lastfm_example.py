23530074b3ecd0544c276039ed86bcdff6e7b43d
amit-sharma <amit_sharma@live.com>
2014-02-05 14:44:16 -0500

    Removed old explore_lastfm.py filr

--- examples/lastfm_example.py
+++ examples/lastfm_example.py
@@ -1,10 +1,16 @@
+""" Example file to demonstrate use of the API.
+
+"""
 from socintpy.caller.get_api import get_api, get_args
 from socintpy.networkcrawl.bfs_networkcrawler import BFSNetworkCrawler
-import settings
+import settings # Local settings file, not in repo for security reasons
 import logging
 
+# Set the log level
 numeric_loglevel = getattr(logging, settings.LOG_LEVEL, None)
 logging.basicConfig(filename="socintpy.log", level = numeric_loglevel)
+
+# Fetch api object using the settings from settings.py
 api_args = get_args(settings)
 api = get_api(api_args)
 """
@@ -14,6 +20,9 @@ result = api.get_data(user='amit_ontop', method="user.getRecentTracks",
 print result      
 """
 
+# Set up the data crawl
 crawler = BFSNetworkCrawler(api, store_type="couchdb", recover = True)
+
+# Start the data crawl
 crawler.crawl(seed_nodes=["jrs1991"], max_nodes = 10)
 
