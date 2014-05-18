382b24785f68f2b7f7a21b416eac8c37f657433e
amit-sharma <amit_sharma@live.com>
2014-05-18 19:43:12 -0400

    towards parallelizing: map was too slow, changed to individual processes

--- socintpy/networkcompute/recommender_analyzer.py
+++ socintpy/networkcompute/recommender_analyzer.py
@@ -1,7 +1,7 @@
 import socintpy.networkcompute.network_recommender as recsys
 from socintpy.networkcompute.basic_network_analyzer import BasicNetworkAnalyzer
 import logging
-from multiprocessing import Pool
+from multiprocessing import Pool, Process
 
 g_netdata= None
 g_interact_type = None
@@ -10,36 +10,46 @@ g_max_recs_shown = None
 g_nodes_list = None
 
 #def process_nodes(partial_nodes_list, netdata, interact_type, klim, max_recs_shown):
-def process_nodes(v_index):
-    #print "Started process"
-    v = g_nodes_list[v_index]
-    ret_local = None
-    ret_global = None
-    #for v in partial_nodes_list:
-    if v.get_num_interactions(g_interact_type) > 1:
-        r1 = recsys.CircleKNearestRecommender(v, g_netdata, interact_type=g_interact_type, K=g_klim,
+def process_nodes(partial_nodes_list):
+#def process_nodes(v_index):
+    print "Started process"
+    #v = g_nodes_list[v_index]
+    #ret_local = None
+    #ret_global = None
+    local_ret_list = []
+    global_ret_list = []
+    counter = 0
+    for v in partial_nodes_list:
+        if counter % 1000 == 0:
+            print "Starting", counter
+        counter += 1
+        if v.get_num_interactions(g_interact_type) > 1:
+            r1 = recsys.CircleKNearestRecommender(v, g_netdata, interact_type=g_interact_type, K=g_klim,
                                                   max_items=g_max_recs_shown)
-        ret1 = r1.recommend()
-        if ret1 is None:
-            logging.warning("Error in computing local recommendations for %d" % v.uid)
-            #return (-1, -1)
-        else:
-            ret_local = r1.getNDCG()
-            r2 = recsys.GlobalKNearestRecommender(v, g_netdata, interact_type=g_interact_type, K=g_klim,
-                                                  max_items=g_max_recs_shown)
-            ret2 = r2.recommend()
-            if ret2 is None:
-                logging.warning("Error in computing global recommendaitons for %d" % v.uid)
+            ret1 = r1.recommend()
+            if ret1 is None:
+                logging.warning("Error in computing local recommendations for %d" % v.uid)
                 #return (-1, -1)
             else:
-                ret_global = r2.getNDCG()
-                #print "Circle recommendations:", v.uid,r1.recommend()
-                #print "Global recommendations", v.uid, r2.recommend()
-                #ret_local.append(r1.getNDCG())
-                #ret_global.append(r2.getNDCG())
-                #print "User l:wikes: ", v.likes, len(v.friends), comm1[len(comm1)-1], comm2[len(comm2)-1], "\n"
-    #print "Ended process"
-    return (ret_local, ret_global)
+                ret_local = r1.getNDCG()
+                r2 = recsys.GlobalKNearestRecommender(v, g_netdata, interact_type=g_interact_type, K=g_klim,
+                                                  max_items=g_max_recs_shown)
+                ret2 = r2.recommend()
+                if ret2 is None:
+                    logging.warning("Error in computing global recommendaitons for %d" % v.uid)
+                    #return (-1, -1)
+                else:
+                    ret_global = r2.getNDCG()
+                    local_ret_list.append(ret_local)
+                    global_ret_list.append(ret_global)
+                    #      print "Circle recommendations:", v.uid,r1.recommend()
+                    #print "Global recommendations", v.uid, r2.recommend()
+                    #ret_local.append(r1.getNDCG())
+                    #ret_global.append(r2.getNDCG())
+                    #print "User l:wikes: ", v.likes, len(v.friends), comm1[len(comm1)-1], comm2[len(comm2)-1], "\n"
+    print "Ended process"
+    #return (ret_local, ret_global)
+    return local_ret_list, global_ret_list
 
  
 class RecommenderAnalyzer(BasicNetworkAnalyzer):
@@ -92,24 +102,35 @@ class RecommenderAnalyzer(BasicNetworkAnalyzer):
         nodes_list = self.netdata.get_nodes_list(should_have_friends=True, should_have_interactions=True)
         g_nodes_list = nodes_list
         num_processes = 2
-        #partial_num_nodes = len(nodes_list) / 4 + 1
-        #arg_list = []
-        #for i in range(num_processes):
-        #    start_index = i * partial_num_nodes
-        #    end_index = min((i + 1) * partial_num_nodes, len(nodes_list))
-        #    arg_list.append(nodes_list[start_index:end_index])
+        partial_num_nodes = len(nodes_list) / num_processes + 1
+        arg_list = []
+        for i in range(num_processes):
+            start_index = i * partial_num_nodes
+            end_index = min((i + 1) * partial_num_nodes, len(nodes_list))
+            arg_list.append(nodes_list[start_index:end_index])
         
-        pool = Pool(processes=num_processes) 
-        print len(nodes_list)
+        #pool = Pool(processes=num_processes)
+        #print len(nodes_list)
         #param_iterable = map(arg_list, [self.netdata]*num_processes, [interact_type]*num_processes,
         #               [klim]*num_processes, [self.max_recs_shown]*num_processes)
-        res_list = pool.map(process_nodes, range(len(nodes_list)), chunksize=10000)
-        pool.close()
-        pool.join()
+        #res_list = pool.map(process_nodes, range(len(nodes_list)), chunksize=10000)
+        #pool.close()
+        #pool.join()
+        #q = queue
+        proc_list = []
+        for i in range(num_processes):
+            p = Process(target=process_nodes, args=(arg_list[i]))
+            p.start()
+            proc_list.append(p)
+        for p in proc_list:
+            p.join()
+
+        """
         for ret_local, ret_global in res_list:
             if ret_local is not None and ret_global is not None:
                 comm1.append(ret_local)
                 comm2.append(ret_global)
+        """
         #pool = Pool(processes=4)
         #pool.map()
         return comm1, comm2
