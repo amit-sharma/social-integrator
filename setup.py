30ba43a1122f2fb6353fd8e85495158682b0950a
amit-sharma <amit_sharma@live.com>
2014-05-18 20:54:36 -0400

    added setup.py for project

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='SocIntPy',
+    version='0.1.0',
+    author='Amit Sharma',
+    author_email='as2447@cornell.edu',
+    packages=['socintpy', 'socintpy.test'],
+    scripts=['bin/tes'],
+    url='http://pypi.python.org/pypi/SocIntPy/',
+    license='LICENSE.txt',
+    description='Here is socintpy',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "matplotlib >= 1.1.1",
+        "numpy >= 0.1.4",
+    ],
+)
