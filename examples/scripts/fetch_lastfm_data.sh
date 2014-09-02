98dc1339f0ff2055251b8a8c46add6b5095c60ba
amit-sharma <amit_sharma@live.com>
2014-09-02 11:22:45 -0400

    changed lastfm fetch script

--- examples/scripts/fetch_lastfm_data.sh
+++ examples/scripts/fetch_lastfm_data.sh
@@ -1,2 +1,2 @@
-python ~/code/social-integrator/examples/crawl/lastfm_fixedcrawler.py -n /home/as2447/code/nodes_list/nodes_to_crawl_part$1 -s $2 -d /home/as2447/datasets/lastfm/part$1/ -a $3
+python ~/code/social-integrator/examples/crawl/lastfm_fixedcrawler.py -n /home/as2447/code/nodes_list/nodes_to_crawl_part$1 -s $2 -d /home/as2447/datasets/lastfm/part$1/ -a $3 || { uname -n; echo "Execution not done for part " $1; } | mail -s "Server status is not good" amit.ontop+server@gmail.com
 { uname -n; echo "Execution done for part " $1; } | mail -s "Server status for data collection" amit.ontop+server@gmail.com 
